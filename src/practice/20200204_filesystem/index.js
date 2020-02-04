58b2793970135c171b3dcaa236492cbfd932faa2
lukas.sin <lukas.sin@kakaocorp.com>
2020-02-04 11:21:42 +0900

    ADD methods and Modify contents in templates

--- src/practice/20200204_filesystem/index.js
+++ src/practice/20200204_filesystem/index.js
@@ -1,1 +1,25 @@
-console.log("hello practice");
+const fs = require("fs"); // filesystme
+const path = require("path"); // filesystme
+
+// get templates
+const getTemplate = $path => {
+  console.log("getTemplate", $path);
+};
+
+// create file
+const createFile = ($path, content) => {
+  console.log("createFile", $path, content);
+};
+
+// create folder
+const createFolder = ($path, name) => {
+  console.log("createFolder", $path, name);
+};
+
+// create
+const create = () => {
+  console.log("create");
+};
+
+// 실행부
+create();
